9d6b6cb4ee66823ceac5df2521b2375bf1c390f9
AroaAyuso <aroa.ayuso@gmail.com>
2019-06-28 12:42:58 +0200

    el tiled de los 4 primeros niveles + imagenes del fondo

--- practicaFinal.js
+++ practicaFinal.js
@@ -1,5 +1,30 @@
+window.addEventListener("load",function() {
+
+    var Q = Quintus()                          // Create a new engine instance
+    .include("Sprites, Scenes, Input, Touch, UI, 2D, TMX, Anim, Audio") // Load any needed modules
+    .setup({width:272, height:224})     // Add a canvas element onto the page
+    .controls()                         // Add in default controls (keyboard, buttons)
+    .touch()                            // Add in touch support (for the UI)
+    .enableSound();                     // Permite activar el sonido
+
+    // CARGA
+    Q.loadTMX("level1.tmx", function() {
+        
+    });
+
+    // NIVEL 1
+    Q.scene("level1", function(stage) {
+        Q.stageTMX("level1.tmx", stage);
+    });
 
 
 
 
 
+
+
+
+
+
+
+});
